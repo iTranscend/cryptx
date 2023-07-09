f99651b584cbb987710d45ae4a893194d6c543d2
iTranscend <llordbanks500@gmail.com>
2023-07-09 19:04:05 +0100

    chore: cleanup prints

--- src/ciphers.rs
+++ src/ciphers.rs
@@ -4,14 +4,12 @@ pub mod encrypt {
         for c in text.chars() {
             let shifted_ascii_number: u8;
             let char_ascii = c as u8;
-            println!("{char_ascii}");
             if c.is_lowercase() {
                 shifted_ascii_number = (char_ascii + shift - 97) % 26 + 97;
             } else if c.is_uppercase() {
                 shifted_ascii_number = (char_ascii + shift - 65) % 26 + 65;
             } else {
                 shifted_ascii_number = char_ascii + shift;
-                println!("{shifted_ascii_number}");
             }
             cipher.push(char::from(shifted_ascii_number));
         }

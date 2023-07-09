6824faa9bbda354c5d435f522e9e80a668160c17
iTranscend <llordbanks500@gmail.com>
2023-07-09 19:00:32 +0100

    feat: caesar cypher decrypter

--- src/ciphers.rs
+++ src/ciphers.rs
@@ -18,3 +18,34 @@ pub mod encrypt {
         cipher
     }
 }
+
+/// Decryption functions 
+pub mod decrypt {
+    /// Decrypter for a Caesar cipher.
+    pub fn caesar(cipher: String, shift: i8) -> String {
+        let mut plaintext = String::from("");
+        for c in cipher.chars() {
+            let shifted_ascii_number: i8;
+            let char_ascii = c as i8;
+            if c.is_lowercase() {
+                let direction = (char_ascii - shift - 97) % 26;
+                if direction.is_negative() {
+                    shifted_ascii_number = 122 + 1 + direction;
+                } else {
+                    shifted_ascii_number = 97 + direction;
+                }
+            } else if c.is_uppercase() {
+                let direction = (char_ascii - shift - 65) % 26;
+                if direction.is_negative() {
+                    shifted_ascii_number = 90 + 1 + direction;
+                } else {
+                    shifted_ascii_number = 65 + direction;
+                }
+            } else {
+                shifted_ascii_number = char_ascii - shift;
+            }
+            plaintext.push(char::from(shifted_ascii_number as u8));
+        }
+        plaintext
+    }
+}

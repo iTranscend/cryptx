20825e383e1928cdfc08e5df05cc34ed958e7a4c
iTranscend <llordbanks500@gmail.com>
2023-07-10 10:10:17 +0100

    rename project to cryptx

--- tests/ciphers.rs
+++ tests/ciphers.rs
@@ -1,11 +1,11 @@
 #[test]
 fn ceasar_encrypt() {
-    let result = encryptx::ciphers::encrypt::caesar("grFthaxy.".to_string(), 4);
+    let result = cryptx::ciphers::encrypt::caesar("grFthaxy.".to_string(), 4);
     assert_eq!(result, "kvJxlebc2".to_string());
 }
 
 #[test]
 fn ceasar_decrypt() {
-    let result = encryptx::ciphers::decrypt::caesar("kvJxlebc2".to_string(), 4);
+    let result = cryptx::ciphers::decrypt::caesar("kvJxlebc2".to_string(), 4);
     assert_eq!(result, "grFthaxy.".to_string());
 }

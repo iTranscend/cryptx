d30d5ad3aa2d32d478e68879b9e8b936a21bd14c
iTranscend <llordbanks500@gmail.com>
2023-07-15 23:37:56 +0100

    modify tests to account for error types

--- tests/ciphers.rs
+++ tests/ciphers.rs
@@ -1,15 +1,20 @@
+use cryptx::errors::CipherError;
+
 #[test]
 fn ceasar_encrypt() {
-    let result = cryptx::ciphers::encrypt::caesar("grFthaxy.".to_string(), 4);
-    assert_eq!(result, "kvJxlebc2".to_string());
+    let result = cryptx::ciphers::encrypt::caesar("grFthaxy.^".to_string(), 4);
+    assert_eq!(result.unwrap(), "kvJxlebc2b".to_string());
 }
 
 #[test]
 fn ceasar_decrypt() {
+    let result = cryptx::ciphers::decrypt::caesar("kvJxlebc2b".to_string(), 4);
+    assert_eq!(result.unwrap(), "grFthaxy.^".to_string());
+}
+
 #[test]
 fn polyalphabetic_encrypt() {
-    let result =
-        cryptx::ciphers::encrypt::polyalphabetic("Theendisalmosthere".into(), "stand");
-    assert_eq!(result, "Mbfsrwctopfithlxlf")
+    let result = cryptx::ciphers::encrypt::polyalphabetic("Theendisalmosthere".into(), "stand");
+    assert_eq!(result.unwrap(), "Mbfsrwctopfithlxlf");
 }
 }

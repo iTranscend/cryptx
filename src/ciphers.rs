ff64e587eb992db919986df950b7060eb8ab14dd
iTranscend <llordbanks500@gmail.com>
2023-07-28 19:05:59 +0100

    feat: polyalphabetic cipher decrypter

--- src/ciphers.rs
+++ src/ciphers.rs
@@ -49,7 +49,11 @@ pub mod encrypt {
 
 /// Decryption functions
 pub mod decrypt {
-    use crate::errors::CipherError;
+    use crate::errors::CipherError::{self, InvalidKeyLength, OnlyAlphabetsAllowed};
+    use crate::types::LoopingIter;
+    use crate::utils::{
+        contains_only_alphabets, shift_character_backward, string_to_aplhabetic_vec,
+    };
 
     /// Decrypter for a Caesar cipher.
     pub fn caesar(cipher: String, shift: u8) -> Result<String, CipherError> {
@@ -60,8 +64,32 @@ pub mod decrypt {
         }
         Ok(plaintext)
     }
-            plaintext.push(char::from(shifted_ascii_number as u8));
+
+    /// Decrypter for Polyalphabetic cipher
+    pub fn polyalphabetic(cipher: String, key: &str) -> Result<String, CipherError> {
+        let mut plaintext = String::from("");
+
+        if !contains_only_alphabets(key) {
+            return Err(OnlyAlphabetsAllowed);
         }
+
+        if key.is_empty() {
+            return Err(InvalidKeyLength);
+        }
+
+        // Get numeric key representation for shifting
+        let key_digits: Vec<u8> = string_to_aplhabetic_vec(key);
+
+        // convert key_digits into a looping iterator
+        let mut looping_key_digits = LoopingIter::new(key_digits);
+
+        // iterate over all ciphertext chars and shift backwards using key_digits_looping iterator
+        for c in cipher.chars() {
+            let shift = looping_key_digits.next().unwrap();
+            let shifted_ascii_number = shift_character_backward(c, shift);
+            plaintext.push(char::from(shifted_ascii_number));
+        }
+
         Ok(plaintext)
     }
 }
